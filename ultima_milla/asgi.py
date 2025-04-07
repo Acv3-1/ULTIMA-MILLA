9ab7513035916a5731cf51d8473db4c4d6b5f210
Acv3-1 <multiwafle@gmail.com>
2025-04-06 21:31:56 -0500

    PRIMER COMIT

---
+++ ultima_milla/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ultima_milla project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ultima_milla.settings')
+
+application = get_asgi_application()
